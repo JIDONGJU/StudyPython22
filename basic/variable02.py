36337220830442f5274b4d63c5c62a103630ba0f
JIDONGJU <wlehdwn7110@naver.com>
2022-05-03 09:10:11 +0900

    220502 파이썬 기본 문법학습

--- basic/variable02.py
+++ basic/variable02.py
@@ -1,39 +1,15 @@
-print(id(a))
-print(id(b))
-print(id(c))
+변수값 = 10
 
+print(변수값)
 
+a = 10
+b = 20
+c = 30
 
-a= 10
-b= 20
-c= 30
-
-print(a==b)
+print(a == b)
 print(a is b)
 
 a = b
-print(a==b)
+print(a == b)
+
 print(a is b)
-
-# 데이터타입
-a = 0b101011
-b = 0b11111111
-print(a)
-print(b)
-print(bin(10))
-
-print(0o52)
-print(oct(10))
-
-print(0xff)
-
-salery = 4_500_500 # 통화단위
-print(salery)
-
-print(type(1))
-print(type(3.14))
-z = False
-print(type(z))
-
-z = 10
-print(type(z))
