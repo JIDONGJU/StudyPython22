6159acc4b2cd4ccd653eb7a933d201c167285fbf
JIDONGJU <wlehdwn7110@naver.com>
2022-07-11 16:31:44 +0900

    20220711 월요일 커밋

--- testproj/testapp/models.py
+++
@@ -1,8 +0,0 @@
-from unittest.util import _MAX_LENGTH
-from django.db import models
-
-# Create your models(=테이블) here.
-# class name == Table name
-class Curriculum(models.Model):
-    # 변수이름 == 컬럼이름
-    name = models.CharField(max_length=255)
--- testproj/testapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('oneshow/', views.oneshow),
-    path('show/', views.show),
-    path('insert/', views.insert),
-    path('index1/', views.index1),
-]
