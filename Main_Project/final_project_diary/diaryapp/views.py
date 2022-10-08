b0e633fe5abedd07806bf5bef97b03400c31f7e3
JIDONGJU <104615410+JIDONGJU@users.noreply.github.com>
2022-10-08 17:43:59 +0900

    Update views.py

--- Main_Project/final_project_diary/diaryapp/views.py
+++ Main_Project/final_project_diary/diaryapp/views.py
@@ -62,7 +62,7 @@ def main_login(request):
     apiKey = ""
     lang = 'kr' #언어
     units = 'metric' #화씨 온도를 섭씨 온도로 변경
-    api = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&lang={lang}&units={units}"
+    api = f""
     result = requests.get(api)
     result = json.loads(result.text)
 
